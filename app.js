d12c8cac3bd109abaaa147b0cbdc2a8ddd4ed23e
Troy M. Connor <troy0820@users.noreply.github.com>
2015-03-14 19:23:52 -0400

    Added bower components to front end modified app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/', routes);
 app.use('/users', users);
 
