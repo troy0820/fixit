977341e395f4819f372275b7b18d610428f198df
Troy Connor <troy0820@gmail.com>
2015-03-16 23:27:32 -0400

    Added _.pluck to get arrays I needed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var _ = require('lodash');
 
 router.get('/', function(req, res) {
  var url = "https://seeclickfix.com/api/v2/issues?place_url=hampton-city&state=VA";
@@ -9,7 +10,11 @@ router.get('/', function(req, res) {
  		console.error(err);
  	}
  	var list = JSON.parse(body).issues;
- 	res.render('index', { title: 'Fix it | Hampton Roads', list: list });
+ 	var lat = _.pluck(list,'lat');
+ 	var lng = _.pluck(list, 'lng');
+ 	var summary = _.pluck(list, 'summary');
+ 	var status = _.pluck(list, 'status');
+ 	res.render('index', { title: 'Fix it | Hampton Roads', list: list, lat:lat, lng:lng, summary:summary, status:status });
 });
 
   
