d44d67a8224958c988938feeb2ab0f346ebc4a42
Troy M. Connor <troy0820@users.noreply.github.com>
2015-04-01 23:30:38 -0400

    Added Post route logic

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,20 @@ router.get('/', function(req, res) {
  	var lng = _.pluck(list, 'lng');
  	var summary = _.pluck(list, 'summary');
  	res.render('index', { title: 'Fix it | Hampton', list: list, lat:lat, lng:lng, summary:summary });
+  });
 });
 
 router.post('/',function(req,res){
-    res.send('Just a post route');
+  var url = "https://seeclickfix.com/api/v2/issues?place_url=hampton-city&state=VA";
+  request(url, function(err, response, body) {
+    if(err){
+      console.error(err);
+    }
+    var list = JSON.parse(body).issues;
+    var lat = _.pluck(list,'lat');
+    var lng = _.pluck(list, 'lng');
+    var summary = _.pluck(list, 'summary');
+    res.render('index', { title:'New City', list:list, lat:lat, lng:lng, summary:summary });
   });
 
 
