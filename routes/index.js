38909f28417441c3c410c4d75bfeae77f8708547
Troy Connor <troy0820@gmail.com>
2015-05-12 20:36:37 -0400

    Added pagination route for city

--- routes/index.js
+++ routes/index.js
@@ -47,7 +47,33 @@ router.get('/:city', function(req, res) {
 });
 
 router.get('/:city/:id',function(req,res) {
-	res.send('This is the page you are on '+ req.params.id);
+	var city = req.params.city;
+  var id = req.params.id;
+  var url = "https://seeclickfix.com/api/v2/issues?place_url="+ city +"&state=VA&per_page=20&page=" + id;
+ 
+   request(url, function(err, response, body) {
+    if(err) {
+      console.error(err);
+    }
+    var list = JSON.parse(body).issues;
+    var metadata = JSON.parse(body).metadata;
+    var per_page = metadata.pagination.per_page;
+    var pages = metadata.pagination.pages;
+    var lat = _.pluck(list,'lat');
+    var lng = _.pluck(list, 'lng');
+    var summary = _.pluck(list, 'summary');
+    
+    if (city == 'newport-news') {
+      city = "newport news";
+    } else if (city == 'virginia-beach') { 
+      city = "virginia beach";
+    }
+    else {
+    city = city.split('-');
+    city = city[0];
+    }
+    res.render('index', { title: city, list: list, lat:lat, lng:lng, summary:summary, per_page:per_page, pages:pages });
+    });
 });
 
 router.post('/',function(req,res){
