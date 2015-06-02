318ea49a960c4c0bd5742d4b7eb4a7bf1abfff88
Troy Connor <troy0820@gmail.com>
2015-06-01 20:15:25 -0400

    Deleted comment in code

--- routes/index.js
+++ routes/index.js
@@ -77,18 +77,17 @@ router.get('/:city/:id',function(req,res) {
 });
 
 router.post('/',function(req,res){
-  //req.body.city = city name from drop down
-  var city = req.body.city;
-  var url = "https://seeclickfix.com/api/v2/issues?place_url=" + city + "&state=VA"; //insert city in this link
-  request(url, function(err, response, body) {
-    if(err){
-      console.error(err);
-    }
-    var list = JSON.parse(body).issues;
-    var lat = _.pluck(list,'lat');
-    var lng = _.pluck(list, 'lng');
-    var summary = _.pluck(list, 'summary');
-    res.render('index', { title: city, list:list, lat:lat, lng:lng, summary:summary });
+    var city = req.body.city;
+    var url = "https://seeclickfix.com/api/v2/issues?place_url=" + city + "&state=VA"; //insert city in this link
+    request(url, function(err, response, body) {
+      if(err){
+        console.error(err);
+      }
+      var list = JSON.parse(body).issues;
+      var lat = _.pluck(list,'lat');
+      var lng = _.pluck(list, 'lng');
+      var summary = _.pluck(list, 'summary');
+      res.render('index', { title: city, list:list, lat:lat, lng:lng, summary:summary });
   });
 
 
