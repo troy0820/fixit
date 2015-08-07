c31af2a6ed4fed254f7e78510520a13dddf359a1
Troy Connor <troy0820@gmail.com>
2015-08-06 23:25:04 -0400

    Cleaned up files for formatting.

--- rp.js
+++ rp.js
@@ -3,16 +3,16 @@
 }());
 
 var rp = require('request-promise');
-var _ = require('lodash');
-var Promise = require('bluebird');
-var geocode = require('geocoder');
-var zips = [];
-var url = "https://seeclickfix.com/api/v2/issues?place_url=hampton&state=VA&per_page=20&page=1";
+    _ = require('lodash');
+    Promise = require('bluebird');
+    geocode = require('geocoder');
+    url = "https://seeclickfix.com/api/v2/issues?place_url=hampton&state=VA&per_page=20&page=1";
+
 rp(url)
    .then(function(body) {
 		var list = JSON.parse(body).issues;
-		var lat = _.pluck(list, 'lat');
-		var lng = _.pluck(list, 'lng');
+		    lat = _.pluck(list, 'lat');
+		    lng = _.pluck(list, 'lng');
 		
 		var lat_lng = { //make an object to return to next async func.
 			lat: lat,
@@ -24,11 +24,11 @@ rp(url)
    })
    	.then(function(lat_lng) {
    		var getzips = function(lat, lng) {
-   		return new Promise(function(resolve, reject) {
-   			geocode.reverseGeocode(lat, lng, function(err, data) {
-      			if (err) { reject(err);}			
-			 	 var result = data.results[0].address_components[6].short_name;
-		      	 resolve(result);
+   			return new Promise(function(resolve, reject) {
+   				geocode.reverseGeocode(lat, lng, function(err, data) {
+      				if (err) { reject(err); }			
+			 	 		var result = data.results[0].address_components[6].short_name;
+		      	 			resolve(result);
 							});
 		   				});
 		   			};
@@ -36,13 +36,13 @@ rp(url)
 		Promise.all(lat_lng.lat.map(function(_,index) {      
    			return getzips(lat_lng.lat[index],lat_lng.lng[index]);
    				})
-		   		).then(function(data) {
-		   			var zips = _.union(data);
-		   		 	console.log('These are the zip codes',zips);
-		   		 });		   	
-   	})
-   	.finally(function() {
-   		console.log('We are finally done here');   		
+		   	).then(function(data) {
+		   		var zips = _.union(data);
+		   		 console.log('These are the zip codes',zips);
+		   	});		   	
+   		})
+   		 .finally(function() {
+   			console.log('We are finally done here');   		
    	
   })
    	.catch(console.error);
