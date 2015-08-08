0433c211c9d13e236830afc65e9360feae8672b7
Troy Connor <troy.connor@yahoo.com>
2015-08-08 14:00:11 -0400

    changed to issues.

--- request-promise.js
+++ request-promise.js
@@ -6,7 +6,7 @@ promise.promisifyAll(request);
 
 request.getAsync("https://seeclickfix.com/api/v2/issues?place_url=hampton&state=VA&per_page=20&page=1")
 		.spread(function(res,body) { //callback signature is (err, res, body) so we need to use spread instead of then
-			var data = JSON.parse(body).metadata;
+			var data = JSON.parse(body).issues;
 			return data;
 		}).then(function(data) {
 			console.log('\n I got the data to this point \n',data);
