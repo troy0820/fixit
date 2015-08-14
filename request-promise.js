07b46281b60b90ff7f338efe673817837325814f
Troy Connor <troy.connor@yahoo.com>
2015-08-14 01:53:58 -0400

    added promisified geocode function to get zips

--- request-promise.js
+++ request-promise.js
@@ -18,29 +18,31 @@ request.getAsync("https://seeclickfix.com/api/v2/issues?place_url=hampton&state=
 			    };
 			return lat_lng;
 		}).then(function(lat_lng) {
-			console.log('these are the lats', lat_lng.lat, ' and longs',lat_lng.lng);
-		
+			console.log('these are the lats \n', lat_lng.lat,  '\n these are the longs \n',lat_lng.lng);
+		})
+		.then(function() { 
 
 //how to promisify geocode 	
 promise.promisifyAll(geocode);
 
-				var getzips = function(lat,lng) {
-					geocode.reverseGeocodeAsync(lat,lng)
-					  .then(function(data) {
-					  	var zips = data.results[0].address_components[6].short_name;
-					  	return zips;
-					  })
-					  .then(function(zips) {
-					  	console.log('we got the zips ',zips);
-					  })
-					  .catch(function(err) {
-					  	console.log(err);
-					  });
-					};
+	var getzips = function(lat,lng) {
+		return new promise(function(resolve, reject) {
+		geocode.reverseGeocodeAsync(lat,lng)
+		  .then(function(data) {
+		  	var zips = data.results[0].address_components[6].short_name;
+		  	resolve(zips);
+		  })
+		})
+		 .catch(function(err) {
+		  	console.log(err);
+		  });
+		};
 
 	promise.all(lat_lng.lat.map(function(_,index) {
 			return getzips(lat_lng.lat[index],lat_lng.lng[index]);
 			})
-	);
-
+		).then(function(data) {
+			var zipcodes = _.union(data);
+			console.log('zipcodes:(uniqued) ',zipcodes);
+		})
 });
