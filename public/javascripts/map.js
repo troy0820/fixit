66a730f70ae9c34354c33589a7fc0259ecf00e62
Troy Connor <troy.connor@yahoo.com>
2015-08-28 23:41:25 -0400

    Undid the file so I can edit the raw and minify whenever

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,1 +1,62 @@
-function success(r){var a=r.coords,e=L.marker([a.latitude,a.longitude],{icon:redIcon});e.bindPopup("This is me"),e.addTo(map)}function error(r){console.warn("ERROR("+r.code+"): "+r.message)}var RedIcon=L.Icon.Default.extend({options:{iconUrl:"/images/marker-icon-red.png"}}),redIcon=new RedIcon,map=L.map("map").setView(["<%= list[0].lat %>","<%= list[0].lng %>"],11),options={enableHighAccuracy:!0,timeout:5e3,maximumAge:0};navigator.geolocation.getCurrentPosition(success,error,options),navigator.geolocation.watchPosition(success,error,options),L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);var lngth="<%= list.length %>",arr=[];art=[],summ=[],arr="<%=lat.slice() %>",art="<%=lng.slice() %>",summ="<%= summary.slice() %>",addr="<%= address.slice() %>";for(var summary=summ.split(","),array=arr.split(","),array2=art.split(","),markers=new L.MarkerClusterGroup,i=0;lngth>i;i++){var marker=L.marker(new L.LatLng(array[i],array2[i]),{});L.circle([array[i],array2[i]]).setRadius(1609.34).addTo(map),marker.bindPopup("Summary: "+summary[i]+"<br/>"),markers.addLayer(marker)}map.addLayer(markers);
+
+        var RedIcon = L.Icon.Default.extend({
+          options: {
+                iconUrl: '/images/marker-icon-red.png'
+          }
+       });
+       var redIcon = new RedIcon();
+
+    var map = L.map('map').setView(["<%= list[0].lat %>", "<%= list[0].lng %>"], 11);
+
+    var options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+function success(pos) {
+var crd = pos.coords;
+
+var me = L.marker([crd.latitude,crd.longitude],{ icon: redIcon })
+        me.bindPopup('This is me')
+        me.addTo(map);
+};
+
+function error(err) {
+console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+navigator.geolocation.watchPosition(success, error, options);
+
+  // // add an OpenStreetMap tile layer
+     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+     }).addTo(map);
+
+      var lngth = "<%= list.length %>";
+      var arr = []
+          art = []
+          summ = []
+          arr = "<%=lat.slice() %>";
+          art = "<%=lng.slice() %>";
+          summ = "<%= summary.slice() %>";
+          addr ="<%= address.slice() %>";
+          var summary = summ.split(',');
+          var array = arr.split(',');
+          var array2 = art.split(',');
+          var markers = new L.MarkerClusterGroup();
+
+        for(var i= 0; i < lngth; i++) {
+             var marker = L.marker(new L.LatLng(array[i], array2[i]), {
+          });
+            L.circle([array[i],array2[i]]).setRadius(1609.34).addTo(map); //radius around marker should be a mile. Can change if need be.
+            marker.bindPopup('Summary: '+summary[i] + '<br/>');
+            markers.addLayer(marker);
+          }
+
+        map.addLayer(markers);
+        /*        for (var i = 0; i < lngth; i++) {
+               marker = new L.marker([array[i],array2[i]])
+         .bindPopup('Summary: ' + summary[i] )
+         .addTo(map); */
