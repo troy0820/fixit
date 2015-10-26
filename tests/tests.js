232ade795664c3a1579d3bf52abbf1ada2920a33
Troy Connor <troy.connor@yahoo.com>
2015-10-25 20:10:57 -0400

    added first test to project

--- tests/tests.js
+++ tests/tests.js
@@ -2,3 +2,11 @@ var request = require('supertest');
     app = require('../app.js');
 
 
+describe('/GET /', function(){
+	it('should render page with external api',function(done){
+		request(app)
+		.get('/')
+		.expect(200, 'oops something went wrong');
+		done();
+	});
+});
