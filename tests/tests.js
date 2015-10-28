3de091f75de01e9062f843a3e4fad61ee5e2d1bb
Troy M. Connor <troy0820@users.noreply.github.com>
2015-10-27 20:17:46 -0400

    added some tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
     app = require('../app.js');
-
+    assert = require('assert'); 
 
 describe('/GET /', function(){
 	it('should render page with external api',function(done){
@@ -10,3 +10,25 @@ describe('/GET /', function(){
 		done();
 	});
 });
+
+describe('/Redirect', function(){
+	it('should redirect',function(done){
+		request(app)
+		.get('/newport-news')
+		.expect(302, done)
+
+	});
+});
+
+
+describe('render html',function(){
+	it('should render some html ', function(done){
+	request(app)
+    	.get('/')
+    	.end(function(err, result) {
+        	assert(result.text.indexOf("</html>") > 0)//statusCode, 200);
+        done();
+    });
+		
+	})
+})
