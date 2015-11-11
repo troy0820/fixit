6a9819a60b2d24b4738516f961b4e877541a6d64
Troy Connor <troy0820@gmail.com>
2015-11-11 13:46:42 -0500

    changed from long to short_name

--- regeocode.js
+++ regeocode.js
@@ -6,10 +6,10 @@
 var geocode = require('geocoder');
 var _ = require('lodash');
 //list of coords will be better 
-geocode.reverseGeocode(36.8923638688899, -76.3887108205164, function(err, result){
+geocode.reverseGeocode(36.9440524794697, -76.2692382467653, function(err, result){
 	if (err) { 
 		console.error(err);
 	}
-		console.log('result', result.results[0].address_components[5].long_name);
+		console.log('result', result.results[0].address_components[6].short_name);
 	
 })
