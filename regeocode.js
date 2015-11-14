661adeffbd12f3dba57a00ca4cd008e67475cc52
Troy M. Connor <troy0820@users.noreply.github.com>
2015-11-13 19:01:32 -0500

    JSON response is inconsistent, can't find postal code solution

--- regeocode.js
+++ regeocode.js
@@ -6,10 +6,10 @@
 var geocode = require('geocoder');
 var _ = require('lodash');
 //list of coords will be better 
-geocode.reverseGeocode(36.9440524794697, -76.2692382467653, function(err, result){
+geocode.reverseGeocode(36.8361511244855, -76.2994304496789, function(err, result){
 	if (err) { 
 		console.error(err);
 	}
-		console.log('result', result.results[0].address_components[6].short_name);
+		console.log('result', result.results[0].address_components);
 	
 })
