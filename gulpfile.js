89f9bee4c870fcb435dde23322f5cbd919ab4ba5
Troy Connor <troy.connor@yahoo.com>
2015-12-03 23:45:51 -0500

    Added gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var shell = require('shelljs');
 
 
 gulp.task('compress',function() {
@@ -9,7 +10,11 @@ gulp.task('compress',function() {
 	.pipe(gulp.dest('.'))
 	
 });
-	
+
+gulp.task('test',function() {
+	shell.exec('npm test');
+});
+
 gulp.task('default',['compress'], function() {
 	console.log('Done Minifying');
 });
