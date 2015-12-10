75f729e4835054145152000abb3a7b56b7f4c7cf
Troy M. Connor <troy0820@users.noreply.github.com>
2015-12-09 23:41:50 -0500

    [linted]Linted code with eslint-config-trails

--- rp.js
+++ rp.js
@@ -1,34 +1,33 @@
 (function() {
-	"use strict";
+	'use strict';
 })();
 
-var rp = require("request-promise");
-var  _ = require("lodash");
-var Promise = require("bluebird");
-var geocode = require("geocoder");
-var url = "https://seeclickfix.com/api/v2/issues?place_url=portsmouth&state=VA&per_page=20&page=1";
+const rp = require('request-promise');
+const  _ = require('lodash');
+const Promise = require('bluebird');
+const geocode = require('geocoder');
+const url = 'https://seeclickfix.com/api/v2/issues?place_url=portsmouth&state=VA&per_page=20&page=1';
 
 rp(url)
    .then(function(body) {
-	var list = JSON.parse(body).issues;
-	var	lat = _.pluck(list, "lat");
-	var	lng = _.pluck(list, "lng");
-		
-	var lat_lng = { //make an object to return to next async func.
+	const list = JSON.parse(body).issues;
+	const lat = _.pluck(list, 'lat');
+	const lng = _.pluck(list, 'lng');
+	const latLng = { //make an object to return to next async func.
 		lat: lat,
 		lng: lng
 	};
-	console.log("lats \n" + lat_lng.lat + "\n longs \n" + lat_lng.lng); 
-	return lat_lng;
+	console.log('lats \n' + latLng.lat + '\n longs \n' + latLng.lng);
+	return latLng;
    })
-   .then (function(lat_lng) {
-	var getzips = function(lat, lng) {
+   .then (function(latLng) {
+	const getzips = function(lat, lng) {
 		return new Promise(function(resolve, reject) {
 			geocode.reverseGeocode(lat, lng, function(err, data) {
 				if (err) { reject(err); }
-				var zips = data.results[0].address_components;
-				var newzips = _.reduce(zips,function(all,item) {
-					if (item.types[0] == "postal_code") {
+				const zips = data.results[0].address_components;
+				const newzips = _.reduce(zips,function(all,item) {
+					if (item.types[0] == 'postal_code') {
 						all.push(item.short_name);
 					}
 					return all;
@@ -36,13 +35,13 @@ rp(url)
 				resolve(newzips);
 			});
 		});
-	};   	
-	Promise.all(lat_lng.lat.map(function(_,index) {
-		return getzips(lat_lng.lat[index],lat_lng.lng[index]);
+	};
+	Promise.all(latLng.lat.map(function(_,index) {
+		return getzips(latLng.lat[index],latLng.lng[index]);
 	})
 		).then(function(data) {
-			var zips = _.union(_.flatten(data));
-			console.log("These are the zip codes",zips.toString());
+			const zips = _.union(_.flatten(data));
+			console.log('These are the zip codes',zips.toString());
 		});
 })
  .catch(console.error);
