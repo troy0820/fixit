03f278c3c4f98e12bb7fda99171388142918fa5a
Troy Connor <troy0820@gmail.com>
2016-01-09 17:34:45 -0500

    [app/package]updated deps and option on urlencode

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+var options = {
+'extended': 'true'
+};
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded(options));
 app.use(compression());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
