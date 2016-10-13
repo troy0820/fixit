a838c070425d0f798adac36162473dbae602d519
Troy Connor <troy0820@gmail.com>
2016-10-13 15:06:45 -0400

    [dashboard]Added nodejs-dashboard to project

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression');
-
+require('nodejs-dashboard');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
